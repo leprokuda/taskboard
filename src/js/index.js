2fd23eb277c6d145d0683bb2f7d34219488362cb
leprokuda <leprokuda@gmail.com>
2022-07-18 18:20:07 +0700

    Рабочий todo без localstorage

---
+++ src/js/index.js
@@ -0,0 +1,146 @@
+const dom = {
+  new: document.getElementById('new'),
+  btnAdd: document.getElementById('add'),
+  tasksList: document.getElementById('tasks'),
+  count: document.getElementById('count')
+}
+
+// Массив задач
+
+const tasksList = []
+
+// Отслеживаем клик по кнопке Добавить задачу
+
+dom.btnAdd.onclick = () => {
+
+  const newTaskText = dom.new.value
+
+  if (newTaskText && isNotHaveTask(newTaskText, tasksList)) {
+    addTask(newTaskText, tasksList)
+    dom.new.value = ''
+    tasksListRender(tasksList)
+  }
+}
+
+// Функция добавления задачи
+
+function addTask(text, list) {
+
+  const timeStamp = Date.now()
+  const newTask = {
+    id: timeStamp,
+    text,
+    isComplete: false
+  }
+
+  list.push(newTask)
+}
+
+// Проверка существования одинаковых задач
+
+function isNotHaveTask(text, list) {
+
+  let isNotHave = true
+
+  list.forEach((newTask) => {
+    if (newTask.text === text) {
+      alert('Такая задача уже существует!')
+      isNotHave = false
+    }
+  })
+
+  return isNotHave
+}
+
+// Функция вывода списка задач (отрисовка)
+
+function tasksListRender(list) {
+
+  let htmlList = ''
+
+  list.forEach((newTask) => {
+    
+    const addClass = newTask.isComplete
+    ? 'taskboard__task taskboard__task_complete'
+    : 'taskboard__task'
+
+    const isChecked = newTask.isComplete
+    ? 'checked'
+    : ''
+
+    const taskHtml = `
+    <div id="${newTask.id}" class="${addClass}">
+      <label class="taskboard__checkbox">
+        <input type="checkbox" ${isChecked}>
+        <div class="taskboard__checkbox-container"></div>
+      </label>
+      <div class="taskboard__task-text">${newTask.text}</div>
+      <div class="taskboard__task-del">-</div>
+    </div>
+    `
+
+    htmlList = htmlList + taskHtml
+
+    renderTasksCount(list)
+  })
+
+  dom.tasksList.innerHTML = htmlList
+}
+
+// Отслеживаем клик по чекбоксу задачи, находим id и статус
+
+dom.tasksList.onclick = (ev) => {
+
+  const target = ev.target
+  const isCheckboxEl = target.classList.contains('taskboard__checkbox-container')
+  const isDeleteEl = target.classList.contains('taskboard__task-del')
+  
+  if (isCheckboxEl) {
+
+    const task = target.parentElement.parentElement
+    const taskId = task.getAttribute('id')
+
+    changeTaskStatus(taskId, tasksList)
+
+    tasksListRender(tasksList)
+  }
+
+  if (isDeleteEl) {
+
+    const task = target.parentElement
+    const taskId = task.getAttribute('id')
+
+    deleteTask(taskId, tasksList)
+  }
+  
+}
+
+// Функция изменения статуса задачи
+
+function changeTaskStatus(id, list) {
+
+  list.forEach((task) => {
+
+    if (task.id == id) {
+      task.isComplete = !task.isComplete
+    }
+  }) 
+}
+
+
+// Функция удаления задачи
+
+function deleteTask(id, list) {
+
+  list.forEach((task, idx) => {
+    if (task.id == id) {
+      list.slpice(idx, 1)
+    }
+  })
+}
+
+// Вывод количества задач
+
+function renderTasksCount(list) {
+  dom.count.innerHTML = list.length
+}
